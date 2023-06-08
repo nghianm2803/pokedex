1449950415c1fa0c1f24eb92860d0c65ed2ddd46
nghia <nghianm2803@gmail.com>
2023-06-08 09:53:44 +0700

    add search api

--- routes/pokemon.api.js
+++ routes/pokemon.api.js
@@ -37,11 +37,49 @@ getDataPokemon();
  * method: get
  */
 router.get("/", (req, res, next) => {
+  const allowedFilter = ["Name", "Type1", "Type2"];
+
   try {
+    let { page, limit, ...filterQuery } = req.query;
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 10;
+    //allow title,limit and page query string only
+    const filterKeys = Object.keys(filterQuery);
+    filterKeys.forEach((key) => {
+      if (!allowedFilter.includes(key)) {
+        const exception = new Error(`Query ${key} is not allowed`);
+        exception.statusCode = 401;
+        throw exception;
+      }
+      if (!filterQuery[key]) delete filterQuery[key];
+    });
+
+    //Number of items skip for selection
+    let offset = limit * (page - 1);
+
     // Read data from db.json then parse JSobject
     let db = fs.readFileSync("db.json", "utf-8");
     db = JSON.parse(db);
     const { pokemon } = db;
+
+    //Filter data by title
+    let result = [];
+
+    if (filterKeys.length) {
+      filterKeys.forEach((condition) => {
+        result = result.length
+          ? result.filter(
+              (pokemon) => pokemon[condition] === filterQuery[condition]
+            )
+          : pokemon.filter(
+              (pokemon) => pokemon[condition] === filterQuery[condition]
+            );
+      });
+    } else {
+      result = pokemon;
+    }
+    //then select number of result by offset
+    result = result.slice(offset, offset + limit);
 
     //send response
     res.status(200).json(pokemon);
